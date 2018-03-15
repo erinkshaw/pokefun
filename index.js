7af70dc35fd5a45a36b5025522aa2aa0c725ee4c
Erin Shaw <erin.kimberly.shaw@gmail.com>
2018-03-15 15:12:21 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+setInterval(rotate, 10)
+
+function rotate() {
+  const deg = 'deg'
+  let currentDeg = Number(getComputedStyle(document.body).getPropertyValue(`--deg`).slice(0, -3)) //.slice(0, -3))
+  console.log(currentDeg)
+  currentDeg = (currentDeg + 1) % 360
+  console.log(currentDeg)
+  document.documentElement.style.setProperty(`--deg`, currentDeg + deg)
+}
