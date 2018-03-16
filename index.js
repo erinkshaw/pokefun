3084d006f316d0d1f4ca9302011e60d5b8113221
Erin Shaw <erin.kimberly.shaw@gmail.com>
2018-03-16 14:04:26 -0400

    added pokefun link

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ setInterval(rotate, 10)
 
 function rotate() {
   const deg = 'deg'
-  let currentDeg = Number(getComputedStyle(document.body).getPropertyValue(`--deg`).slice(0, -3)) //.slice(0, -3))
+  let currentDeg = Number(getComputedStyle(document.body).getPropertyValue(`--deg`).slice(0, -3))
   console.log(currentDeg)
   currentDeg = (currentDeg + 1) % 360
   console.log(currentDeg)
